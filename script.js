cc48e892af2b80c3e2f2f092e52901378f4f0028
josh <chrisjosh087@gmail.com>
2024-07-31 13:25:08 +0100

    Modified

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const sideMenu = document.querySelector('#sideMenu');
-
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav ul");
 function openMenu(){
 
     sideMenu.style.transform = 'translateX(-16rem)';
@@ -9,3 +10,18 @@ function closeMenu(){
 
     sideMenu.style.transform = 'translateX(16rem)';
 }
+
+window.addEventListener('scroll', () => {
+    if (scrollY > 50) {
+        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.remove("bg-white", "shadow-sm", "bg-opacity-50");
+    } else {
+         navBar.classList.remove(
+           "bg-white",
+           "bg-opacity-50",
+           "backdrop-blur-lg",
+           "shadow-sm"
+         );
+         navLinks.classList.add("bg-white", "shadow-sm", "bg-opacity-50");
+    }
+})
